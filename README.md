7d4d61759bd700e12f8efda416b7c80e75b28cbd
Kevin Henderson <umhendek@gmail.com>
2019-05-18 13:04:55 -0500

    added edge styles and improved examples

--- README.md
+++ README.md
@@ -6,17 +6,17 @@ Uses matplotlib and networkx to create graph of given directory structure and ex
 
 ```
 ./pfsg.py /dev/disk dot.png --image /home/odroid/opportunity.png -l dot --file-node-colour "#AA2222FF" --show-files --file-node-size 145 --label-size 12 --label-colour "#CCCCCCFF" --edge-colour "#666666FF" --edge-width 5
-
 ```
 <img src="https://github.com/niveknosredneh/PFSG/blob/master/img/dot.png" width="640" align="middle">
 
 ```
-./pfsg.py /dev twopi.png --image /home/odroid/eclipse.jpg -l twopi
+./pfsg.py /home/odroid/roms twopi.png --image /home/odroid/eclipse.jpg -l twopi
 ```
 <img src="https://github.com/niveknosredneh/PFSG/blob/master/img/twopi.png" width="640" align="middle">
 
 ```
-./pfsg.py /dev/disk sfdp.png --image /home/odroid/Grey-Wall.png -l sfdp --label-depth 12 --file-node-colour "#AA2222FF" --show-files --file-node-size 0 --show-file-labels --label-size 24 --file-label-size 14 --file-label-colour "#FF2222FF" --label-colour "#AA2222FF" --edge-colour "#662222FF" --edge-width 2
+./pfsg.py /home/odroid/Code sfdp.png -b "#000000FF" -l sfdp --edge-colour "#222222FF" --edge-width 1 --label-colour "#FFFFFFFF" -f --file-node-colour "#FF5555FF" --file-node-size 25 --label-size 8
+
 ```
 <img src="https://github.com/niveknosredneh/PFSG/blob/master/img/sfdp.png" width="640" align="middle">
 
@@ -46,31 +46,31 @@ cd PFSG && sudo chmod +x pfsg.py
 ...
 ```
 
-## Options
+### Options
 ```
-  -w, --width INTEGER            Graph width
-  -h, --height INTEGER           Graph height
-  --image TEXT                   Image file for graph to overlay ontop of
-  -b, --background-colour TEXT   Background colour in form '#RRGGBBAA'
-  -f, --label-size INTEGER       Font size for labels
-  --label-colour TEXT            Font colour in form '#RRGGBBAA'
-  --label-depth INTEGER          Max depth to draw labels
-  -f, --file-label-size INTEGER  Font size for file labels
-  --file-label-colour TEXT       Font colour in form '#RRGGBBAA'
-  --edge-width FLOAT             Edge line width
-  --edge-colour TEXT             Edge colour in form '#RRGGBBAA'
-  --node-size INTEGER            Node dot size, set to zero to disable
-  --file-node-size INTEGER       Node dot size, set to zero to disable
-  --node-colour TEXT             Node colour in form '#RRGGBBAA'
-  --file-node-colour TEXT        Node colour in form '#RRGGBBAA'
-  --show-files                   Shows files
-  --show-file-labels             Shows file labels
-  -l, --layout TEXT              Layout style: ‘dot’, ‘twopi’, ‘fdp’, ‘sfdp’,
-                                 ‘circo’
-  --set-wallpaper                Sets output image as wallpaper using feh
-  -v, --verbose                  Display more output than necessary
-  --help                         Show this message and exit.
-
+  -w, --width INTEGER           Graph width
+  -h, --height INTEGER          Graph height
+  -l, --layout TEXT             Layout style: dot|twopi|neato|fdp|sfdp|circo
+  -i, --image TEXT              Image file for graph to overlay ontop of
+  -b, --background-colour TEXT  Background colour in form '#RRGGBBAA'
+  -d, --depth INTEGER           Max node depth
+  --label-depth INTEGER         Max depth to draw labels
+  --label-size INTEGER          Font size for labels
+  --label-colour TEXT           Font colour in form '#RRGGBBAA'
+  --file-label-size INTEGER     Font size for file labels
+  --file-label-colour TEXT      Font colour in form '#RRGGBBAA'
+  --edge-width FLOAT            Edge line width
+  --edge-colour TEXT            Edge colour in form '#RRGGBBAA'
+  --edge-style TEXT             Edge style: solid|dashed|dotted|dashdot
+  --node-size INTEGER           Node dot size, set to zero to disable
+  --node-colour TEXT            Node colour in form '#RRGGBBAA'
+  --file-node-size INTEGER      Node dot size, set to zero to disable
+  --file-node-colour TEXT       Node colour in form '#RRGGBBAA'
+  -f, --show-files              Shows file nodes
+  --show-file-labels            Shows file labels, restricted by label-depth
+  -s, --set-wallpaper           Sets output image as wallpaper using feh
+  -v, --verbose                 Display more output than necessary
+  --help                        Show this message and exit.
 
 ```
 
--- pfsg.py
+++ pfsg.py
@@ -14,34 +14,35 @@ import click
 @click.argument("output", nargs=1)
 @click.option("--width", "-w",  default=1920, help="Graph width")
 @click.option("--height", "-h",  default=1080, help="Graph height")
-@click.option("--image",  default=None, help="Image file for graph to overlay ontop of")
-@click.option("--background-colour", "-b",   default="#33333300", help="Background colour in form '#RRGGBBAA'")
-@click.option("--label-size", "-f",  default=12, help="Font size for labels")
+@click.option("--layout", "-l",  default="dot", help="Layout style: dot|twopi|neato|fdp|sfdp|circo ")
+@click.option("--image",  "-i",  default=None, help="Image file for graph to overlay ontop of")
+@click.option("--background-colour", "-b",   default="#333333FF", help="Background colour in form '#RRGGBBAA'")
+@click.option("--depth",  "-d", default=48, help="Max node depth")
+@click.option("--label-depth",  default=10, help="Max depth to draw labels")
+@click.option("--label-size",  default=12, help="Font size for labels")
 @click.option("--label-colour",  default="#AAAAAAFF", help="Font colour in form '#RRGGBBAA'")
-@click.option("--label-depth",  default=2, help="Max depth to draw labels")
-@click.option("--file-label-size", "-f",  default=8, help="Font size for file labels")
+@click.option("--file-label-size", default=8, help="Font size for file labels")
 @click.option("--file-label-colour",  default="#AAAAAAFF", help="Font colour in form '#RRGGBBAA'")
 @click.option("--edge-width", default=0.5, help="Edge line width")
 @click.option("--edge-colour", default="#555555FF", help="Edge colour in form '#RRGGBBAA'")
+@click.option("--edge-style", default="solid", help="Edge style: solid|dashed|dotted|dashdot ")
 @click.option("--node-size", default=0, help="Node dot size, set to zero to disable")
-@click.option("--file-node-size", default=0, help="Node dot size, set to zero to disable")
 @click.option("--node-colour", default="#555555FF", help="Node colour in form '#RRGGBBAA'")
-@click.option("--file-node-colour", default="#555555FF", help="Node colour in form '#RRGGBBAA'")
-@click.option("--show-files", default=False, is_flag=True, help="Shows files")
-@click.option("--show-file-labels", default=False, is_flag=True, help="Shows file labels")
-@click.option("--layout", "-l",  default="dot", help="Layout style: ‘dot’, ‘twopi’, ‘fdp’, ‘sfdp’, ‘circo’")
-@click.option("--set-wallpaper", default=False, is_flag=True, help="Sets output image as wallpaper using feh")
+@click.option("--file-node-size", default=0, help="Node dot size, set to zero to disable")
+@click.option("--file-node-colour", default="#FF5555FF", help="Node colour in form '#RRGGBBAA'")
+@click.option("--show-files", "-f",  default=False, is_flag=True, help="Shows file nodes")
+@click.option("--show-file-labels", default=False, is_flag=True, help="Shows file labels, restricted by label-depth")
+@click.option("--set-wallpaper", "-s",  default=False, is_flag=True, help="Sets output image as wallpaper using feh")
 @click.option("--verbose", "-v",  default=False, is_flag=True, help="Display more output than necessary")
 def main(
-directory, output, 
-width, height, 
+directory, output, layout, 
+width, height, depth, 
 label_size, label_colour, label_depth, 
 background_colour, image,  
-edge_colour, edge_width,  
+edge_colour, edge_width, edge_style, 
 node_colour, node_size, 
 show_files, file_node_colour, file_node_size, file_label_colour, file_label_size, show_file_labels, 
-layout,  set_wallpaper, 
-verbose):
+set_wallpaper, verbose):
     """ 
     PFSG - Python Filesystem Grapher
     
@@ -50,18 +51,21 @@ verbose):
     """
     G = nx.Graph() # create graph
     base_depth = len(directory.split("/"))  -1
-    G.add_node(directory,  file='False',  depth=0,  label=directory)
+    G.add_node(directory,  file='False',  depth=0,  label=directory.split("/")[-1])
     for (dirpath, dirnames, filenames) in os.walk(directory):
-        for f in filenames:
-            G.add_node(os.path.join(dirpath, f),  file='True',  depth=(len(dirpath.split("/"))-base_depth),  label=f)
-            G.add_edge(dirpath ,  os.path.join(dirpath, f))
+        if show_files:
+            for f in filenames:
+                G.add_node(os.path.join(dirpath, f),  file='True',  depth=(len(dirpath.split("/"))-base_depth),  label=f)
+                G.add_edge(dirpath ,  os.path.join(dirpath, f), file='True')
         for d in dirnames:
-            G.add_node(os.path.join(dirpath, d),  file='False', depth=(len(dirpath.split("/"))-base_depth),  label=d)
-            G.add_edge(dirpath ,  os.path.join(dirpath, d))
+                G.add_node(os.path.join(dirpath, d),  file='False', depth=(len(dirpath.split("/"))-base_depth),  label=d)
+                G.add_edge(dirpath ,  os.path.join(dirpath, d),  file='False')
           
     file_nodes = []
-    dir_nodes = []
+    file_edges = []
     file_labels = {}
+    dir_nodes = []
+    dir_edges = []
     dir_labels = {}
     files=nx.get_node_attributes(G,'file')
     labels=nx.get_node_attributes(G,'label')
@@ -70,16 +74,23 @@ verbose):
         if file=='True':
             file_nodes.append(node)
             if depth[node] < label_depth: file_labels[node] = labels[node]
-        else:
+        else: # for directories
             dir_nodes.append(node)
             if depth[node] < label_depth: dir_labels[node] = labels[node]
+    
+    files=nx.get_edge_attributes(G,'file')
+    for edge, file in files.items():
+        if file=='True':
+            file_edges.append(edge)
+        else: # for directories
+            dir_edges.append(edge)
     
     pos = graphviz_layout(G, prog=layout, root=1) # use layout
     fig = plt.figure(figsize=(width/100.0, height/100.0)) # set figure size
     
-    nx.draw_networkx_nodes(G, pos, nodelist=dir_nodes, node_size=node_size, node_color=node_colour)
+    nx.draw_networkx_nodes(G, pos, nodelist=dir_nodes, node_size=node_size, node_color=node_colour,  cmap=plt.get_cmap('Reds'))
     nx.draw_networkx_nodes(G, pos, nodelist=file_nodes, node_size=file_node_size, node_color=file_node_colour)
-    nx.draw_networkx_edges(G, pos, edge_color=edge_colour, width=edge_width)#,  style='dotted',  connectionstyle='arc3,rad=0.2')
+    nx.draw_networkx_edges(G, pos, edge_color=edge_colour, width=edge_width,  style=edge_style)
     if show_file_labels: nx.draw_networkx_labels(G, pos, labels=file_labels ,  font_color=file_label_colour, font_size=file_label_size,  alpha=1)
     nx.draw_networkx_labels(G, pos, labels=dir_labels ,  font_color=label_colour, font_size=label_size,  alpha=1)
     plt.axis('off')
