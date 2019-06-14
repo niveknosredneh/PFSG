2f59a5c20e63de5c8386baad8fc8b3a130cbe7dc
Kevin Henderson <umhendek@gmail.com>
2019-06-14 15:39:35 -0500

    removed dependencies, more stable, faster

--- README.md
+++ README.md
@@ -2,63 +2,21 @@
 
 Uses matplotlib, networkx and graphviz to create a beautiful graph of directory structure and exports as a png image for use as desktop wallpaper
 
-## Examples
-
-```
-./pfsg.py ~/polybar dot.png --image /home/odroid/opportunity.png \
--l dot --file-node-colour "#BBBBBBFF" --show-files \
---file-node-size 15 --node-size 15 --label-size 20 \
---label-colour "#FFFFFFFF" --edge-colour "#999999FF" \
---edge-style dotted --edge-width 3 --label-depth 1
-```
-<img src="https://github.com/niveknosredneh/PFSG/blob/master/img/dot.png" width="640" align="middle">
-
-```
-./pfsg.py ~/Code twopi.png -b "#000000FF" \
--l twopi --edge-colour "#666666FF" --edge-width 1 \
---label-colour "#FFFFFFFF" -f --file-node-colour "#55FF55FF"\
- --file-node-size 5 --label-depth 3
-```
-<img src="https://github.com/niveknosredneh/PFSG/blob/master/img/twopi.png" width="640" align="middle">
-
-```
-./pfsg.py ~/Code sfdp.png -b "#000000FF" -l sfdp \
---edge-colour "#222222FF" --edge-width 1 \
---label-colour "#FFFFFFFF" -f --file-node-colour "#FF5555FF" \
---file-node-size 25 --label-size 8
-```
-<img src="https://github.com/niveknosredneh/PFSG/blob/master/img/sfdp.png" width="640" align="middle">
-
-```
-./pfsg.py ~/polybar ~/neato.png --image ~/Grey-Wall.png \
--w 1920 -h 1080 -l neato --edge-width 2 --edge-width-reduce 0.2 \
---file-node-size 3 --node-size 0 --label-size-reduce 2 \
---edge-colour inferno --colourmap-soften 5 \
---label-alpha-reduce 0.15
-```
-<img src="https://github.com/niveknosredneh/PFSG/blob/master/img/neato.png" width="640" align="middle">
-
 ## Prerequisites
 
 Tested only on ubuntu 18.04
 
 Ubuntu and Debian based:
 ```
-# Mandatory dependencies:
-sudo apt-get install python3 python3-pip
-sudo pip3 install matplotlib networkx pydot graphviz click
-
-# Optional dependencies:
-sudo apt-get install imagemagick feh
+# Dependencies:
+sudo apt-get install python3 python3-pip imagemagick feh
+sudo pip3 install networkx graphviz click
 ```
 (untested) Arch based:
 ```
 # Mandatory dependencies:
-sudo pacman -S python3 python3-pip python-pygraphviz
-sudo pip3 install matplotlib networkx pydot graphviz click
-
-# Optional dependencies:
-sudo pacman -S imagemagick feh
+sudo pacman -S python3 python3-pip python-pygraphviz imagemagick feh
+sudo pip3 install networkx graphviz click
 ```
 
 ## Installing
@@ -78,33 +36,27 @@ cd PFSG && sudo chmod +x pfsg.py
   -b, --background-colour TEXT  Background colour in form '#RRGGBBAA'
   -d, --max-depth INTEGER       Max node depth
   -f, --show-files              Shows file nodes
-  --show-file-labels            Shows file labels, restricted by label-depth
-  --label-depth INTEGER         Max depth to draw labels
+  --show-file-labels            Shows file labels
   --label-size INTEGER          Font size for labels
   --label-size-reduce FLOAT     Amount to reduce label size by depth
-  --label-alpha INTEGER         Label alpha
-  --label-alpha-reduce FLOAT    Amount to reduce label alpha by depth
   --label-colour TEXT           Font colour in form '#RRGGBBAA'
+  --label-depth INTEGER         Max depth to draw labels
   --file-label-size INTEGER     Font size for file labels
   --file-label-colour TEXT      Font colour in form '#RRGGBBAA'
   --edge-width FLOAT            Edge line width
   --edge-width-reduce FLOAT     Amount to reduce edge width by depth
-  --edge-colour TEXT            Edge colour '#RRGGBBAA' or matplotlib colormap
+  --edge-length FLOAT           Minimum edge length in inches
+  --edge-colour TEXT            Edge colour in form '#RRGGBBAA'
   --edge-style TEXT             Edge style: solid|dashed|dotted|dashdot
-  --node-size INTEGER           Node dot size, set to zero to disable
-  --node-colour TEXT            Node colour '#RRGGBBAA' or matplotlib colormap
-  --file-node-size INTEGER      Node dot size, set to zero to disable
+  --splines TEXT                Edge style: none|line|curved|ortho
+  --node-size FLOAT             Node dot size, set to zero to disable
+  --node-colour TEXT            Node colour in form '#RRGGBBAA'
+  --file-node-size FLOAT        Node dot size, set to zero to disable
   --file-node-colour TEXT       Node colour in form '#RRGGBBAA'
-  --colourmap-soften TEXT       Adds buffer to colourmap range
-  -m, --map-by-size             Maps colours by file size rather than depth
+  --dim INTEGER                 Number of dimensions to graph in
   -s, --set-wallpaper           Sets output image as wallpaper using feh
-  -v, --verbose                 Display more output than necessary
   --help                        Show this message and exit.
 ```
-
-<img src="https://matplotlib.org/_images/sphx_glr_colormap_reference_003.png" width="640" align="middle">
-
-See [https://matplotlib.org](https://matplotlib.org/gallery/color/colormap_reference.html) for full colourmap reference
 
 ## Authors
 
--- pfsg.py
+++ pfsg.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 
-# Kevin Matthew Henderson - May 18, 2019
+# Kevin Matthew Henderson - June 14, 2019
 
-import pydot
 from networkx.drawing.nx_pydot import write_dot
-from networkx.drawing.nx_pydot import graphviz_layout
-import matplotlib
-import matplotlib.pyplot as plt
+
 import networkx as nx
 import os
 import subprocess
@@ -23,51 +20,46 @@ import click
 @click.option("--background-colour", "-b",   default="#222222FF", help="Background colour in form '#RRGGBBAA'")
 @click.option("--max-depth",  "-d", default=48, help="Max node depth")
 @click.option("--show-files", "-f",  default=False, is_flag=True, help="Shows file nodes")
-@click.option("--show-file-labels", default=False, is_flag=True, help="Shows file labels, restricted by label-depth")
-@click.option("--label-depth",  default=10, help="Max depth to draw labels")
-@click.option("--label-size",  default=12, help="Font size for labels")
+@click.option("--show-file-labels", default=False, is_flag=True, help="Shows file labels")
+@click.option("--label-size",  default=16, help="Font size for labels")
 @click.option("--label-size-reduce",  default=1.0, help="Amount to reduce label size by depth")
-@click.option("--label-alpha",  default=0.8, help="Label alpha")
-@click.option("--label-alpha-reduce",  default=0.07, help="Amount to reduce label alpha by depth")
 @click.option("--label-colour",  default="#FFFFFFFF", help="Font colour in form '#RRGGBBAA'")
+@click.option("--label-depth",  default=10, help="Max depth to draw labels")
 @click.option("--file-label-size", default=8, help="Font size for file labels")
 @click.option("--file-label-colour",  default="#AAAAAAFF", help="Font colour in form '#RRGGBBAA'")
-@click.option("--edge-width", default=0.5, help="Edge line width")
-@click.option("--edge-width-reduce", default=0.3, help="Amount to reduce edge width by depth")
-@click.option("--edge-colour", default="#555555FF", help="Edge colour in form '#RRGGBBAA' or      matplotlib colormap, see readme")
+@click.option("--edge-width", default=2.0, help="Edge line width")
+@click.option("--edge-width-reduce", default=0.03, help="Amount to reduce edge width by depth")
+@click.option("--edge-length", default=1.0, help="Minimum edge length in inches")
+@click.option("--edge-colour", default="#555555FF", help="Edge colour in form '#RRGGBBAA'")
 @click.option("--edge-style", default="solid", help="Edge style: solid|dashed|dotted|dashdot ")
-@click.option("--node-size", default=0, help="Node dot size, set to zero to disable")
+@click.option("--splines", default="curved", help="Edge style: none|line|curved|ortho ")
+@click.option("--node-size", default=0.1, help="Node dot size, set to zero to disable")
 @click.option("--node-colour", default="#555555FF", help="Node colour in form '#RRGGBBAA'")
-@click.option("--file-node-size", default=0, help="Node dot size, set to zero to disable")
+@click.option("--file-node-size", default=0.05, help="Node dot size, set to zero to disable")
 @click.option("--file-node-colour", default="#FF5555FF", help="Node colour in form '#RRGGBBAA'")
+@click.option("--dim", default=2, help="Number of dimensions to graph in")
 @click.option("--set-wallpaper", "-s",  default=False, is_flag=True, help="Sets output image as wallpaper using feh")
-@click.option("--verbose", "-v",  default=False, is_flag=True, help="Display more output than necessary")
 def main(
 directory, output, layout, 
 width, height, max_depth, 
-label_size, label_colour, label_depth, label_size_reduce, label_alpha_reduce, label_alpha, 
+label_size, label_colour, label_size_reduce, label_depth, 
 background_colour, image, opacity,   
-edge_colour, edge_width, edge_style, edge_width_reduce, 
+edge_colour, edge_width, edge_style, edge_width_reduce, splines, edge_length, 
 node_colour, node_size, 
 show_files, file_node_colour, file_node_size, file_label_colour, file_label_size, show_file_labels, 
-set_wallpaper, verbose):
+set_wallpaper,  dim):
     """ 
     PFSG - Python Filesystem Grapher
     
-    Uses matplotlib and networkx to create graph of a directory structure and exports to png image for use as desktop wallpaper
+    Uses networkx and graphviz to create graph of a directory structure and exports to png image for use as desktop wallpaper
     """
-
-    if edge_colour[0]=="#": edge_colourmap = None
-    else: edge_colourmap = plt.get_cmap(edge_colour)
-    if node_colour[0]=="#": node_colourmap = None
-    else: node_colourmap = plt.get_cmap(node_colour)
-    
+   
     print("Creating Network and Adding Nodes ...")
-    G = nx.Graph(bgcolor="transparent", dpi=100,  size=f"{width}x{height}") # create graph
+    G = nx.DiGraph( ) # create graph
     base_depth = len(directory.split("/"))
     deepest = 0 # keeps track of max folder depth
     num_nodes = 1
-    G.add_node(directory,  file='False',  depth=0,  label=directory.split("/")[-1])
+    G.add_node(directory,  file='False',  depth=0,  label=directory.split("/")[-1],    shape="plain",  color=node_colour,  fontcolor=label_colour,  fontsize=int(label_size))
     for (dirpath, dirnames, filenames) in os.walk(directory):
         cur_depth = len(dirpath.split("/"))-base_depth + 1
         if cur_depth > deepest: 
@@ -77,63 +69,40 @@ set_wallpaper, verbose):
             if show_files:
                 for f in filenames: # FILES
                     new_label = ""
-                    if cur_depth < label_depth: new_label = f
-                    G.add_node(os.path.join(dirpath, f),  file='True',  depth=cur_depth,  label=new_label,  color=file_node_colour,  fontcolor=label_colour)
-                    G.add_edge(dirpath ,  os.path.join(dirpath, f), file='True',  depth=cur_depth)
+                    shape = "plain"
+                    if cur_depth < label_depth and show_file_labels: 
+                        new_label = f
+                    else: 
+                        shape = "point"
+                    G.add_node(os.path.join(dirpath, f),  file='True',  depth=cur_depth,  label=new_label,   shape=shape,  color=file_node_colour,  fontcolor=file_label_colour,  fontsize=int(file_label_size),  width=file_node_size)
+                    G.add_edge(dirpath ,  os.path.join(dirpath, f), file='True',  style=edge_style, depth=cur_depth,  arrowsize=0.2,  len=edge_length,  color=edge_colour,  penwidth=(edge_width-edge_width_reduce*cur_depth) )
                     num_nodes+=1
             for d in dirnames: # DIRECTORIES
                     new_label = ""
+                    shape = "plain"
                     if cur_depth < label_depth: new_label = d
-                    G.add_node(os.path.join(dirpath, d),  file='False', depth=cur_depth,  label=new_label,  shape="plain",  color=node_colour,  fontcolor=label_colour)
-                    G.add_edge(dirpath ,  os.path.join(dirpath, d),  file='False',  depth=cur_depth,  color=edge_colour)
+                    else: 
+                        shape = "point"
+                    G.add_node(os.path.join(dirpath, d),  file='False', depth=cur_depth,  label=new_label,  shape=shape,  color=node_colour,  fontcolor=label_colour,  fontsize=int(label_size - label_size_reduce * cur_depth), width=node_size)
+                    G.add_edge(dirpath ,  os.path.join(dirpath, d),  file='False', style=edge_style, depth=cur_depth,  arrowsize=0.2,  len=edge_length,  color=edge_colour,  penwidth=(edge_width-edge_width_reduce*cur_depth))
                     num_nodes+=1
 
     print("Added " + str(num_nodes) + " Nodes")
     write_dot(G, 'graph.dot')
     
-    """
-    print("Calculating Layout ...")
-    pos = graphviz_layout(G, prog=layout)#, root=1) # use layout
-    fig = plt.figure(figsize=(width/100.0, height/100.0)) # set figure size
-    
-    # DRAW
-    print("Drawing Graph ...")
-    #nodes
-    node_depth = nx.get_node_attributes(G,'depth')
-    edge_depth = nx.get_edge_attributes(G,  'depth')
-    node_labels =  nx.get_node_attributes(G,  'label')
-    is_file = nx.get_node_attributes(G,'file')
-    for node, depth in node_depth.items():
-        nx.draw_networkx_nodes(G, pos, nodelist=[node], node_size=node_size, node_color=[depth], cmap=node_colourmap, vmin=0, vmax=deepest)
-    #edges
-    for edge, depth in edge_depth.items():
-            width=(edge_width-depth*edge_width_reduce)
-            if edge_colourmap: edge_colour = [depth]
-            nx.draw_networkx_edges(G, pos, edgelist=[edge], edge_color=edge_colour, width=width, edge_cmap=edge_colourmap, style=edge_style,  edge_vmin=0,edge_vmax=deepest,  alpha=1)
-    #labels
-    for node, label in node_labels.items():
-        new_label_size = int(int(label_size) - int(node_depth[node]) * int(label_size_reduce))
-        print(label)
-        nx.draw_networkx_labels(G, pos, labels={node:label} ,  font_color=label_colour, font_size=new_label_size,  alpha=1)
-    
-    plt.axis('off')
-    #plt.axis('equal')
+    with open(output, "w") as outfile: # uses outputfile as output for neato using stdout
+        subprocess.call(['neato', '-Tpng', '-K'+layout,   f"-Gsplines={splines}",  f"-Gbgcolor={background_colour}", f"-Gdimen={dim}", 'graph.dot'], stdout=outfile)
         
-    if image: fig.set_facecolor('#00000000') # for transparency
-    else: fig.set_facecolor(background_colour)
-    
-    # SAVE
-    print("Saving File ...")
-    plt.savefig(output, facecolor=fig.get_facecolor() )
-    plt.close()
-    """
-    with open(output+"2", "w") as outfile:
-        subprocess.call(['neato', '-Tpng', '-K'+layout,   'graph.dot'], stdout=outfile)
+    out = subprocess.call(['convert',  output,  '-background', background_colour , output])
+    out = subprocess.call(['convert',  output,  '-fuzz',  '20%', '-transparent', background_colour, output])
+        
+    out = subprocess.call(['convert',  output,  "-sample",  f"{width}x{height}!" ,  output])
+
     # depends on imagemagick
     if image:
         print("Overlaying Over Image ...") 
-        out = subprocess.call(['convert',  output,  '-gravity', 'center', '-background', 'white', '-extent',  f'{width}x{height}',  output])
-        out = subprocess.call(['convert',  output,  '-transparent', 'white', output])
+        #out = subprocess.call(['convert',  output,  '-background', 'white', output])
+        #out = subprocess.call(['convert',  output,  '-transparent', 'white', output])
         out = subprocess.call(['composite',  '-blend',  str(opacity),  output, image,  output])
     # depends on feh
     if set_wallpaper: 
